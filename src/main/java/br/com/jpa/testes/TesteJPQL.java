c4c30c7984204ea5c400a6c67dbe3797e2557c4e
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-01-15 21:47:00 -0300

    utilizando TypedQuery

--- src/main/java/br/com/jpa/testes/TesteJPQL.java
+++ src/main/java/br/com/jpa/testes/TesteJPQL.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.jpa.modelo.Conta;
 import br.com.jpa.modelo.Movimentacao;
@@ -17,12 +17,12 @@ public class TesteJPQL {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
 		EntityManager em = emf.createEntityManager();
 		
-		String sql = "select m from Movimentacao m where m.conta = :pConta";
+		String sql = "select m from Movimentacao m where m.conta = :pConta order by m.valor desc";
 		
 		Conta conta = new Conta();
 		conta.setId(1L);
 		
-		Query query = em.createQuery(sql);
+		TypedQuery<Movimentacao> query = em.createQuery(sql, Movimentacao.class);
 		query.setParameter("pConta", conta);
 		
 		List<Movimentacao> movimentacoes = query.getResultList();
