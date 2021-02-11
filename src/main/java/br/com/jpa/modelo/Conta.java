b2d593e4d0200f103e90332034f3ea2e1f3d2250
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-02-11 08:25:40 -0300

    evoluindo

--- src/main/java/br/com/jpa/modelo/Conta.java
+++ src/main/java/br/com/jpa/modelo/Conta.java
@@ -3,6 +3,7 @@ package br.com.jpa.modelo;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -19,7 +20,7 @@ public class Conta {
 	private String titular;
 	private Double saldo;
 	
-	@OneToMany
+	@OneToMany(mappedBy = "conta", fetch = FetchType.EAGER)
 	private List<Movimentacao> movimentacoes;
 
 	public Long getId() {
